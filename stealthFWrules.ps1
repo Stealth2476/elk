d7254c5341f576d00b0fb06f6d381f6ad4f2d7e5
UISsajata <timothy.saja@gmail.com>
2023-07-12 12:10:26 -0400

    Update stealthFWrules.ps1
    turned windows FW on and turned on FW logging

--- stealthFWrules.ps1
+++ stealthFWrules.ps1
@@ -1,6 +1,13 @@
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
+Set-NetFirewallProfile -Name Domain -LogBlocked True
+Set-NetFirewallProfile -Name Domain -LogAlloweded True
+Set-NetFirewallProfile -Name Domain -LogIgnored True
 New-NetFirewallRule -DisplayName "~ Stealth SCIP 51294" -Direction Inbound -Action Allow -LocalPort 51294 -Protocol UDP
 New-NetFirewallRule -DisplayName "~ Stealth SCIP 51295" -Direction Inbound -Action Allow -LocalPort 51295 -Protocol UDP
 New-NetFirewallRule -DisplayName "~ Stealth IKE v4" -Direction Inbound -Action Allow -LocalPort 500 -Protocol UDP
 New-NetFirewallRule -DisplayName "~ Stealth IPSEC NAT-PAT" -Direction Inbound -Action Allow -LocalPort 4500 -Protocol UDP
 New-NetFirewallRule -DisplayName "~ Stealth IPSEC protocol 50" -Direction Inbound -Action Allow -Protocol 50
 New-NetFirewallRule -DisplayName "~ Stealth ICMP" -Direction Inbound -Action Allow -Protocol 1
+#  Uncomment for SSD Server
+# New-NetFirewallRule -DisplayName "~ Stealth SSD WFP Logs 5099" -Direction Inbound -Action Allow -LocalPort 5099 -Protocol TCP
+# New-NetFirewallRule -DisplayName "~ Stealth SSD iptables Logs 5098" -Direction Inbound -Action Allow -LocalPort 5098 -Protocol TCP
